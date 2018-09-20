daeb21f6514af3c27df579fdedd913252bcce06e
at821 <36107231+at821@users.noreply.github.com>
2018-09-20 16:02:32 -0400

    Catching Apple Section

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    //Prefab for instantiation apples
+    public GameObject applePrefab;
+
+    //speed at which the Apple tree moves
+    public float speed = 1f;
+
+    //Distance where AppleTress turns around
+    public float leftAndRightEdge = 10f;
+    
+    //Chance that the Appletree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+        //rate at which apples will be instantiated
+        public float secondsBetweenAppleDrops = 1f;
+
+    void Start ()    {
+        //Dropping apples every second
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+    void Update() {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //changing direction
+        if ( pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed); //Move right
+        }
+        else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed); // Moves left
+        }
+     }   
+    
+    void FixedUpdate() {
+
+     //changing direction rendomly is now time-based because of FixedUpdate ()
+        if (Random.value < chanceToChangeDirections) {
+
+            speed *= -1; //change direction
+        }
+
+    }
+   
+    
+
+}
---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+	
+	void Update () {
+        //get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //the cammera's z position sets how far to push the mouse into 3d
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move the x posiition of this Basket to the zPosition of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+	}
+
+    void OnCollisionEnter(Collision coll) {
+        //find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple") {
+            Destroy(collidedWith);
+        }
+    }
+}
